bde72a63dc739a45563701bf3fb7b5d12481a7e7
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-07-09 07:36:19 +0200

    Added a function that calculates the dimensions and position of the grid and other adjustments

--- src/game.h
+++ src/game.h
@@ -3,9 +3,9 @@
 
 #include "textures.h"
 
-#define GAME_WIDTH  800
-#define GAME_HEIGHT 600
-#define GAME_TITLE  "RayTris"
+#define WIN_WIDTH         800
+#define WIN_HEIGHT        600
+#define GAME_TITLE        "RayTris"
 
 #define GRID_ROWS         20
 #define GRID_COLS         10
@@ -15,6 +15,8 @@
 
 #define SHAPES_COUNT      7
 #define SHAPE_VALS        4
+
+#define BG_COLOR          0x282C34FF
 
 typedef struct {
     int x;
@@ -29,7 +31,7 @@ typedef struct {
 } Shape;
 
 typedef struct {
-    Vector2 pos;
+    Rectangle bounds;
     Texture_Id cells[GRID_ROWS*GRID_COLS];
     int cell_spacing;
 } Grid;
@@ -44,11 +46,12 @@ typedef struct {
 extern Point _shapes[SHAPES_COUNT][SHAPE_VALS];
 
 void game_init(Game* game);
+Grid grid_create(void);
 void game_update(Game* game);
 void game_draw(Game game);
 void grid_draw(Grid grid);
 
-void shape_create(Shape* shape);
+Shape shape_create(void);
 void shape_draw(Vector2 base_pos, Shape shape);
 void shape_move(Grid grid, Shape* shape, int dir);
 void shape_rotate(Grid grid, Shape* shape);
@@ -57,5 +60,7 @@ void shape_lock(Game* game, Shape* shape);
 int check_collisions(Grid grid, int x, int y);
 void grid_clear_row(Grid* grid, int row, unsigned int* new_score);
 int is_row_full(Grid grid, int row);
+
+Rectangle grid_get_bounds(int grid_rows, int grid_cols, int cell_spacing, int cell_size);
 
 #endif /* GAME_H_ */
