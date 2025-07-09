c0663a7b6abc788ada0f41fd65853b747a1f3274
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-07-09 07:40:13 +0200

    main.c prototype

--- src/main.c
+++ src/main.c
@@ -1,13 +1,32 @@
 #include "raylib.h"
 
-int main(void) {
-    InitWindow(800, 600, "RayTris");
+#include "textures.h"
+#include "game.h"
 
-    while (!WindowShouldClose()) {
+#include <time.h>
+
+int main(void) {
+    InitWindow(WIN_WIDTH, WIN_HEIGHT, GAME_TITLE);
+    SetTargetFPS(60);
+
+    load_texture_atlas();
+    SetRandomSeed(time(NULL));
+    Game game = {0};
+    game_init(&game);
+
+    float frame_time = 0.0f;
+    while (!WindowShouldClose() && game.running) {
+        frame_time += GetFrameTime();
+        if (frame_time >= 3/4.0f) {
+            frame_time = 0.0f;
+            game_update(&game);
+        }
         BeginDrawing(); {
-            ClearBackground(GetColor(0x282C34FF));
+            ClearBackground(GetColor(BG_COLOR));
+            game_draw(game);
         } EndDrawing();
     }
+    unload_texture_atlas();
 
     CloseWindow();
     return 0;
