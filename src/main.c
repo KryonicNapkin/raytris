0a457a32f3b9ad5444058a11734d809370a230b3
coevoe <85091338+KryonicNapkin@users.noreply.github.com>
2025-07-11 14:33:57 +0200

    Forgot to add main.c to the previous commit

--- src/main.c
+++ src/main.c
@@ -5,27 +5,30 @@
 
 #include <time.h>
 
+// TODO: End game screen
+// TODO: Shows the next shape
+// DONE: Keep track of the time
+// TODO: Add levels
+
 int main(void) {
     InitWindow(WIN_WIDTH, WIN_HEIGHT, GAME_TITLE);
     SetTargetFPS(60);
 
     load_texture_atlas();
-    load_fonts();
+    load_font();
     SetRandomSeed(time(NULL));
     Game game = {0};
     game_init(&game);
 
-    float frame_time = 0.0f;
     while (!WindowShouldClose() && game.running) {
         handle_input(&game, game_get_input());
         game_update(&game);
         BeginDrawing(); {
             ClearBackground(GetColor(BG_COLOR));
             game_draw(game);
-            draw_score(game.grid.bounds, game.score);
         } EndDrawing();
     }
-    unload_fonts();
+    unload_font();
     unload_texture_atlas();
 
     CloseWindow();
